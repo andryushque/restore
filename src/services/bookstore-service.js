f2d8873c71ebfe407306b2fd5e6714c6ef230256
Andrey Um <a.um@devs-marketing.ru>
2021-04-19 11:21:03 +0300

    Create: react-redux structure

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,5 +1,8 @@
 export default class BookstoreService {
   getBooks() {
-    return [];
+    return [
+      { id: 1, title: "The Gunslinger", author: "Stephen King" },
+      { id: 2, title: "The Drawing of the Three", author: "Stephen King" },
+    ];
   }
 }
