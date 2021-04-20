90fb4d0c3c525f41d5421603633f628f0146c68a
Andrey Um <a.um@devs-marketing.ru>
2021-04-20 10:10:32 +0300

    Create: add/remove actions; update: reducer structure

---
+++ src/reducers/book-list.js
@@ -0,0 +1,39 @@
+const updateBookList = (state, action) => {
+  const { type, payload } = action;
+
+  if (state === undefined) {
+    return {
+      books: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  switch (type) {
+    case "FETCH_BOOKS_REQUEST":
+      return {
+        books: [],
+        loading: true,
+        error: null,
+      };
+
+    case "FETCH_BOOKS_SUCCESS":
+      return {
+        books: payload,
+        loading: false,
+        error: null,
+      };
+
+    case "FETCH_BOOKS_FAILURE":
+      return {
+        books: [],
+        loading: false,
+        error: payload,
+      };
+
+    default:
+      return state.bookList;
+  }
+};
+
+export default updateBookList;
